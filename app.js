20269553cd947cdc5d068279c1f62c3eca0d8424
Onechezzz <maximilian.afanasyev@gmail.com>
2024-08-13 18:42:40 +0300

    ref: deleted getRandomColor func added chroma library changed setRandomColours via chroma added posibility to change elements color depends on luminance

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const cols = document.querySelectorAll('.col')
 
-function generateRandomColor(){
-	let color = '';
-	const hexCodes ='0123456789ABCDEF'
-	for (let i=0;i<6;i++){
-		color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-	}
-	return '#' + color
-}
+// function generateRandomColor(){
+// 	let color = '';
+// 	const hexCodes ='0123456789ABCDEF'
+// 	for (let i=0;i<6;i++){
+// 		color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+// 	}
+// 	return '#' + color
+// }
 
 function setRandomColours(){
 	cols.forEach(col=> {
 		const text = col.querySelector('h2')
-		const color = generateRandomColor()
+		const lockButton = col.querySelector('button')
+		const color = chroma.random()
 		text.textContent = color
 		col.style.background = color
+		
+		setTextColor(text, color)
+		setTextColor(lockButton, color)
 	})
+}
+
+function setTextColor(text, color){
+	const luminance = chroma(color).luminance()
+	text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
 setRandomColours()
