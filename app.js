bcb53d8b6d0bd732c245b38898a765e2918e9c5c
Onechezzz <maximilian.afanasyev@gmail.com>
2024-08-13 17:41:10 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const cols = document.querySelectorAll('.col')
+
+function generateRandomColor(){
+	let color = '';
+	const hexCodes ='0123456789ABCDEF'
+	for (let i=0;i<6;i++){
+		color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+	}
+	return '#' + color
+}
+
+function setRandomColours(){
+	cols.forEach(col=> {
+		col.style.background = generateRandomColor()
+	})
+}
+
+setRandomColours()
