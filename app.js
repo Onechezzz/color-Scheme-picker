76e21c9e0cc9a158b9cb2e9698e784dd5416766b
Onechezzz <maximilian.afanasyev@gmail.com>
2024-08-14 20:18:22 +0300

    feat: addedHash handling updateColorHash, getColorFromHash, copyToClipBoard, EventListeners on 'click' changed setRandomColours logic

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const cols = document.querySelectorAll('.col')
 
 document.addEventListener('keydown', event =>{
+	event.preventDefault()
 	if (event.code.toLowerCase() === 'space'){
 		setRandomColours()
+	}
+})
+
+document.addEventListener('click', event =>{
+	const type = event.target.dataset.type
+	
+	if (type === 'lock'){
+		const node = event.target.tagName.toLowerCase() === 'i'
+			? event.target
+			: event.target.children[0]
+		node.classList.toggle('fa-lock-open')
+		node.classList.toggle('fa-lock')
+	}
+	else if(type === 'copy'){
+		copyToClipboard(event.target.textContent)
 	}
 })
 // function generateRandomColor(){
@@ -14,17 +30,44 @@ document.addEventListener('keydown', event =>{
 // 	return '#' + color
 // }
 
-function setRandomColours(){
-	cols.forEach(col=> {
+function copyToClipboard(text){
+	return navigator.clipboard.writeText(text)
+}
+
+function setRandomColours(isInitial){
+	const colors = isInitial ? getColorsFromHash() : []
+	
+	cols.forEach((col, index)=> {
+		const isLocked = col.querySelector('i').classList.contains('fa-lock')
+		
 		const text = col.querySelector('h2')
 		const lockButton = col.querySelector('button')
-		const color = chroma.random()
+		
+		
+		
+		if(isLocked){
+			colors.push(text.textContent)
+			return
+		}
+		
+		
+		const color = isInitial
+			? colors[index]
+				? colors[index]
+				: chroma.random()
+			: chroma.random()
+		
+		if(!isInitial){
+			colors.push(color)
+		}
+		
 		text.textContent = color
 		col.style.background = color
 		
 		setTextColor(text, color)
 		setTextColor(lockButton, color)
 	})
+	updateColorsHash(colors)
 }
 
 function setTextColor(text, color){
@@ -32,4 +75,21 @@ function setTextColor(text, color){
 	text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
-setRandomColours()
+function updateColorsHash(colors=[]){
+	document.location.hash = colors
+		.map(col => col.toString().substring(1))
+		.join('-')
+	
+}
+
+function getColorsFromHash(){
+	if(document.location.hash.length > 1){
+		return document.location.hash
+			.substring(1)
+			.split('-')
+			.map(color => '#' + color)
+	}
+	return []
+}
+
+setRandomColours(true)
